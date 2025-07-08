22ae67e52c55a09620eb9627cce321b32af830ad
Bryan <mauriciobryan1998@gmail.com>
2025-07-08 20:32:14 -0300

    Redirecting to Room

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,9 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
+type GetRoomsAPIRespose = Array<{
+  id: string;
+  name: string;
+}>;
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-rooms"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/rooms");
+      const result: GetRoomsAPIRespose = await response.json();
+      return result;
+    },
+  });
   return (
     <main>
       <div>Create Room</div>
+      <br />
+
+      {isLoading && <p>Loading...</p>}
+      <div className='flex flex-col gap-2'>
+        {data?.map((room) => {
+          return (
+            <Link key={room.id} to={`room/${room.id}`}>
+              {room.name}
+            </Link>
+          );
+        })}
+      </div>
       <Link className='underline' to='/room'>
         Acessar sala
       </Link>
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,3 +1,12 @@
+import { Navigate, useParams } from "react-router-dom";
+
+type RoomParams = {
+  id: string;
+};
 export function Room() {
+  const { id } = useParams<RoomParams>();
+  if (!id) {
+    return <Navigate replace to='/' />;
+  }
   return <div>Room details</div>;
 }
